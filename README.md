dad2022caba39acc2ce16a329acb88e63ce7ade6
Ivan Lorenzo <ivan.lorenzo@with-madrid.com>
2023-03-23 10:49:43 +0100

    Released state 0.1.0

--- README.md
+++ README.md
@@ -7,17 +7,9 @@ To install this module we need to get it from npm with the following command:
 ```commandline
 npm install @model-w/toast
 ```
+
 ### Setup
-
-
 After we need to add to the `nuxt.config.ts` file, the following param:
 ```typescript
-    runtimeConfig: {
-        public:
-            {
-                sentryDSN: process.env.SENTRY_DSN
-            }
-        }
+modules: ["@model-w/toast"]
 ```
-
-With all these done the sentry must start to track the errors you have
--- playground/app.vue
+++ playground/app.vue
@@ -1,21 +1,19 @@
-<script setup>
-// import { toast } from 'vue3-toastify';
+<script setup lang="ts">
+  import { nextTick } from "@vue/runtime-core";
+  import { useNuxtApp } from "#app";
 
-nextTick(() => {
-  if (process.client) {
-    useNuxtApp().$toast('notify after nextTick');
+  nextTick(() => {
+    if (process.client) {
+      useNuxtApp().$toast('notify after nextTick');
+    }
+  })
+  const notify = () => {
+    useNuxtApp().$toast.info('toastify success');
   }
-});
-
-const notify = () => {
-  useNuxtApp().$toast.info('toastify success');
-  // or
-  // toast.info('toastify success');
-};
 </script>
 
 <template>
   <div>
-    <button @click="notify">notify by click</button>
+    <button @click="notify">Notify by click</button>
   </div>
 </template>
